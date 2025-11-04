21bbe221e99b2820782225cfb98fb727a9f0da2c
Raylandson <www.raylandsoncs@gmail.com>
2025-11-04 17:28:00 -0300

    adding jump precision

--- mario-top/scenes/player/player.gd
+++ mario-top/scenes/player/player.gd
@@ -11,15 +11,18 @@ var friction = 100.0
 var gravity = 10.0
 var jump_height = 5 * TILE_SIZE
 var jump_velocity = sqrt(2*gravity*jump_height)
+var gravity_scale = 1.0
 
 func _physics_process(delta: float) -> void:
 	match actual_state:
 		states.idle:
+			%SpriteAnim.play("idle")
 			velocity.x = max(abs(velocity.x) - friction, 0) * sign(velocity.x)
 			if get_direction() != 0.0:
 				actual_state = states.walk
 			check_jump(delta)
 		states.walk:
+			%SpriteAnim.play("walk")
 			check_jump(delta)
 			var direction = get_direction()
 			var opposite_direction = direction != sign(velocity.x)
@@ -30,8 +33,12 @@ func _physics_process(delta: float) -> void:
 			velocity.x = clamp(velocity.x,-max_speed, max_speed)
 			if get_direction() == 0.0:
 				actual_state = states.idle
-	
-	velocity.y += gravity
+	flip_nodes()
+	if velocity.y > 0 or not Input.is_action_pressed("jump"):
+		gravity_scale = 2.0
+	else:
+		gravity_scale = 1.0
+	velocity.y += gravity * gravity_scale
 	move_and_slide()
 
 
@@ -43,3 +50,8 @@ func check_jump(delta):
 
 func get_direction():
 	return (Input.get_action_strength("right") - Input.get_action_strength("left"))
+
+
+func flip_nodes():
+	if get_direction() != 0:
+		%SpriteAnim.flip_h = get_direction() > 0
